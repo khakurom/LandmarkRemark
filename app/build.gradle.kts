0c36ce37f104b653a5fb93ca8aff2eb74702204f
kha.nvu <ngovukha54@gmail.com>
2024-05-11 11:48:51 +0700

    + Implement ui main screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,9 +38,9 @@ android {
     }
 
     buildFeatures {
-        dataBinding
-        viewBinding
-        buildConfig
+        dataBinding = true
+        viewBinding = true
+        buildConfig = true
     }
 }
 
@@ -51,6 +51,8 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.databinding:databinding-runtime:8.4.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
